a6d1f791e2134bd702504fb04d0f2699329df976
shemarjoseph <shemarjoseph@yahoo.com>
2018-08-17 14:35:01 -0400

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,13 @@
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    results = []
+    self.each do |animal, country|
+      arguments.each do |arg|
+        if country == arg
+          results << animal
+        end
+      end
+    end
+    results
   end
 end
